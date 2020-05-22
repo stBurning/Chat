138e15cb9c02660f2cafc821596b85dc6c9c711d
Устинов Константин <richardocouls@gmail.com>
2020-05-22 15:35:03 +0500

    Add files via upload

---
+++ Chat/Chat/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace Chat {
+    partial class Form1 {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent() {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 41);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(486, 397);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start Server";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(510, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Server";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Chat/Chat/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Chat {
+    public partial class Form1 : Form {
+
+        private Server server;
+        Thread serverThread;
+        public Form1() {
+            InitializeComponent();
+            server = new Server();
+            server.ServerLog += Server_ServerLog;
+            
+        }
+
+        private void Server_ServerLog(string s) {
+            if (!richTextBox1.InvokeRequired) {
+                richTextBox1.AppendText(s + '\n');
+            } else
+                Invoke(new Server.Log(Server_ServerLog), s);
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            
+            button1.Enabled = false;
+            serverThread = new Thread(() => {
+                server.Start();
+            });
+            serverThread.Start();
+              
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e) {
+            
+            server.Stop();
+            if (serverThread != null) {
+                serverThread.Interrupt();
+                Environment.Exit(0); 
+            }
+
+        }
+    }
+
+    
+}
